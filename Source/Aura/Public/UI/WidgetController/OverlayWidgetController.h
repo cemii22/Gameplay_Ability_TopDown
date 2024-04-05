0079c0ebee4942c7f084ae372e8866d1c5fdc370
Ercem <136481968+cemii22@users.noreply.github.com>
2024-04-05 21:44:11 +0200

    listening for attribute changes

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -17,10 +17,15 @@ class AURA_API UOverlayWidgetController : public UAuraWidgetController
 	GENERATED_BODY()
 public:
 	virtual void BroadcastInitialValues() override;
+	virtual void BindCallbacksToDependencies() override;
 
 	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
 	FONHealthChangedSignature OnHealthChanged;		//delegate its self
 
 	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
 	FONMaxHealthChangedSignature OnMaxHealthChanged;
+
+protected:
+	void HealthChanged(const FOnAttributeChangeData& Data) const;
+	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
 };
